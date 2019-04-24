4a289ab24edeb61da974043bdf9ef02f9a9784e7
reisthi <thiagoth.reis@gmail.com>
2019-04-24 10:13:54 -0500

    hackerhank practing

---
+++ diagonal_difference.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the diagonalDifference function below.
+def diagonalDifference(arr):
+    h = len(arr)-1
+    side_one, side_two = 0, 0
+    for a in range(len(arr)):
+        side_one += arr[a][a]
+        side_two += arr[a][h-a]
+    return abs(side_one - side_two)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
