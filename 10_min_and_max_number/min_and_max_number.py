55739c972a2446375b7250dcfd454746c4b58eeb
reisthi <thiagoth.reis@gmail.com>
2021-09-25 13:08:40 -0500

    created utils

--- 10_min_and_max_number/min_and_max_number.py
+++ 10_min_and_max_number/min_and_max_number.py
@@ -5,9 +5,6 @@
 
 """
 
-import random
-from timeit import default_timer as timer
-
 
 def custom_max_min_num(array):
     minimum, maximum = 0, 0
@@ -21,17 +18,3 @@ def custom_max_min_num(array):
 
 def min_max(array):
     return f"max: {max(array)}. min: {min(array)}"
-
-
-def gen_random_list(n, mini, maxi):
-    a_list = list()
-    [a_list.append(random.randint(mini, maxi)) for a in range(0, n)]
-    return a_list
-
-
-def holy(args):
-    start = timer()
-    # gen_random_list()
-    print(custom_max_min_num(args))
-    end = timer()
-    print(f"Finished: {end - start} ms")
---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+import random
+from timeit import default_timer as timer
+
+
+def gen_random_list(n, mini, maxi):
+    a_list = list()
+    [a_list.append(random.randint(mini, maxi)) for a in range(0, n)]
+    return a_list
+
+
+def holy():
+    start = timer()
+    end = timer()
+    print(f"Finished: {end - start} ms")
