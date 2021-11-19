fae80a47aa7f09d4cf5379c260983332248a880e
reisthi <thiagoth.reis@gmail.com>
2021-11-19 00:33:58 -0600

    refactor and review

--- 07_strings/text_warp.py
+++ 07_strings/text_warp.py
@@ -8,12 +8,15 @@ import math
 
 
 def w(string, max_width):
+    if max_width <= 0:
+        return
+
     new_string = str()
     counter, size = 0, max_width
-    iterations = math.ceil(len(string)/max_width)
+    iterations = len(string) // max_width
+
     for a in range(0, iterations):
-        # print(a, type(string[counter:size]))
         new_string += string[counter:size] + '\n'
         counter = size
         size += max_width
-    return new_string
+    print(new_string)
--- 12_sales_by_match/sales_by_match.py
+++ 12_sales_by_match/sales_by_match.py
@@ -1,10 +1,10 @@
 """
-Returns number of pair of socks in an array.
+It must return an integer representing the number of matching pairs of socks that are available
 Example:
         n = 9
         array = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 """
-
+from collections import Counter
 
 def solution(array):
     """ Return the amount of pair of numbers in a list. """
@@ -16,3 +16,13 @@ def solution(array):
             if array.count(item) > 1:
                 amount += int(array.count(item) / 2)
     return amount
+
+
+def solution_2(array):
+    coll = Counter(array)
+    pairs = 0
+
+    for key, value in coll.items():
+        if value > 1:
+            pairs += value // 2
+    return pairs
